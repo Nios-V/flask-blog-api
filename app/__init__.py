f88b172e9fba68a37f58481fe912ae862e6b90ad
Nicolás Cáceres <niosv.1104@gmail.com>
2025-11-09 14:21:53 -0300

    Initial commit: setup project

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_cors import CORS
+from .config import Config
+from .extensions import db, migrate, jwt
+
+def create_app():
+    app.Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+    CORS(app) # TODO: Limit CORS origins
+
+    return app
